fc4d434761c9e02d11b939f77b76c3dbdd28308a
Satyam Shivam Sahu <sss.sahusatyam2000@gmail.com>
2025-09-02 16:05:40 +0530

    endpoint created with streamlit as frontend

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -9,7 +9,8 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 class GraphBuilder():
     def __init__(self):
-        pass
+        self.tools = []
+        self.system_prompt = SYSTEM_PROMPT
 
     def agent_function(self, state:MessagesState):
         """Main agent function"""
